a60290b44a26fbba9d1243d2b8df756ca8d50cab
sorinworld <jsorin1123@gmail.com>
2022-12-28 13:33:42 +0900

    [추가] globalthis, datatypes

--- README.md
+++ README.md
@@ -7,7 +7,8 @@
 학습이 완료된 항목은 체크박스로 표시됩니다.
 
 - [x] Code Structure
-- [ ] Variables
-- [ ] Strict Mode
-
+- [x] Variables
+- [x] Strict Mode
+- [x] global this
+- [x] Data Types
 
---
+++ client/chapter/core/02.variable.js
@@ -0,0 +1,55 @@
+/* --------------------------------------------------------------------- */
+/* Variables                                                             */
+/* --------------------------------------------------------------------- */
+
+console.log('aaa')
+
+/* eslint no-unused-vars: 0 */
+let admin;
+let name;
+name = 'john';
+admin = name;
+console.log(admin);
+
+let OurplanetName = 'Earth';
+let crrentUserName = 'sorinworld';
+
+
+/* 다음 내용을 분석한 후, 프로그래밍 할 수 있도록 변수와 상수를 작성해봅니다. ----------- */
+
+// - 갯수 별 상품 가격 계산하기
+let calcProductQuentity;
+
+let calcEachProductPrice;
+// - 구매 제품 가격의 총 합
+let TotalProductPrice;
+// - 1년 기준 일(day)자 수
+const DAY_PER_YEAR = 365;
+// - 구매 결제 여부
+let isPayment = false;
+let hasClassName = true;
+// - 구매 결제 내역
+let PaymentList;
+// - 브랜드 접두사
+const BRAND_PREFIX = 'EUID';
+// - 오늘의 운세
+let todayLuck;
+let fortuneOfToday;
+// - 상품 정보 : 상황에 따라 다름.
+const productInfo = '';
+let productInformation;
+
+
+
+
+
+/* variables ----------------------------------------------------------- */
+
+
+/* constant variables -------------------------------------------------- */
+
+// redux
+// panding 아무것도 안한상태
+// loading 로딩중, 요청대기
+// fulfilled, resolved 응답성공
+// rejected 실패
